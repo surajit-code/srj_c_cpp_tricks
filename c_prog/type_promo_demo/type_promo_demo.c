ad1bd025265964d27a7298d6712535c324bedc67
Surajit Das <surajit.code@gmail.com>
2020-11-24 09:58:42 +0000

    Another type demotion example added

--- c_prog/type_promo_demo/type_promo_demo.c
+++ c_prog/type_promo_demo/type_promo_demo.c
@@ -10,5 +10,13 @@ int main()
 
     printf("%lf\n", z);
     printf("%d\n", w); //type demotion, inaccurate answer
+
+    printf( "%zu\n", sizeof(int));
+    unsigned int a = 0xFFFFFFFF;
+    printf("a accurate: %u\n", a);
+    printf( "%zu\n", sizeof(short));
+    unsigned short b = a;
+    printf("a inaccurate when stored in b: %u\n", b); //type demotion
+
     return 0;
 }
